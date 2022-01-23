730c4d5bd7d6c00f4e7ef07579e86403165c4ec6
vamosgs <gegham.samvelyan@hotmail.com>
2022-01-24 00:42:26 +0400

    Preloader fix

--- preloader.js
+++ preloader.js
@@ -1,10 +1,14 @@
 const preloader = document.querySelector('.preloader');
+const imgs = document.querySelectorAll('img');
+console.log(imgs);
+
+
 
 document.onreadystatechange = () => {
-  preloader.classList.add('closing');
   if (document.readyState == 'complete') {
+    preloader.classList.add('closing');
     setTimeout(() => {
-      preloader.style.display = "none";
+      preloader.style.display = 'none';
     }, 600);
   }
 };
