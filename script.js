50c0808aa5b92407cfeee2c0b5d83719b940c0d2
vamosgs <gegham.samvelyan@hotmail.com>
2022-01-23 23:34:59 +0400

    Added favicon and popup

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const li = document.querySelectorAll('li');
+const button = document.querySelector('#GET_BUTTON');
+const popup = document.querySelector('.popup');
 
 li.forEach(item => {
   item.setAttribute('data-hover', item.textContent);
 });
+
+button.onclick = () => {
+  popup.classList.add('active');
+};
+
+popup.onclick = item => {
+  if (
+    item.target.classList[0] === 'popup' ||
+    item.target.classList[0] === 'close'
+  )
+    popup.classList.remove('active');
+};
